3f90ea94c6667c0c0f42f625ffa0c02a85d87310
DavidRaHe <david0905.rtf@gmail.com>
2025-04-06 01:55:07 -0600

    Delete src directory

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-export{default as registerRoutes} from "./register.routes.js"
-
-export{default as loginRoutes} from "./login.routes.js"
-
-export{default as productosRoutes} from "./productos.routes.js"
-
-export{default as datosRoutes} from "./datos.routes.js"
-
-export{default as updeteRoutes} from "./updete.routes.js"
--- src/routes/updete.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getDelete, getUpdete } from '../controllers/updetecontroller.js';
-
-const router = Router();
-router.put('', getUpdete);
-router.delete('', getDelete);
-
-export default router;
